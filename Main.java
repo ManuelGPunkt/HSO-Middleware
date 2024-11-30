7bf3dc37b735dcc13e2b41d0a8d5a541485a0084
ManuelGPunkt <130501764+ManuelGPunkt@users.noreply.github.com>
2024-11-30 17:18:06 +0100

    Add files via upload
    initial commit, weil dieses schmutz git nicht einfach mal normal ein Projekt hochladen kann auf meinen Account!!!

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+/**
+ * 
+ */
+
+/**
+ * @author mark
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
